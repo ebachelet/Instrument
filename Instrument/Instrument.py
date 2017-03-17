b67d5aa3aa64cd749efedd51838dc5dd40c9986a
Etienne Bachelet <etibachelet@gmail.com>
2017-03-17 08:57:15 -0700

    First commit

---
+++ Instrument/Instrument.py
@@ -0,0 +1,195 @@
+import sys
+thismodule = sys.modules[__name__]
+
+def define_instrument(name):
+
+
+    try:
+
+        instrument = getattr(thismodule, '{}'.format(name))
+
+    except AttributeError:
+
+        raise InstrumentException('Unknown instrument "{}"'.format(name))
+
+    return instrument()
+
+
+class InstrumentException(Exception):
+     
+	pass
+
+
+
+class Telescope(object):
+
+	
+	 def __init__(self):
+
+
+		pass
+
+
+class Instrument(object):
+
+
+	def __init__(self):
+
+		self.name = None
+		self.type = 'Imager'
+		self.camera_model = None
+		self.ron = None
+		self.gain = None
+		self.pix_scale = None
+		self.x_binning = 1
+		self.y_binning = 1
+		self.fov = None
+		self.ADU_low = None
+		self.ADU_high = None
+		self.bpm = None	
+		self.ro_speed = None			
+		self.header_dictionnary = {}
+		self.telescope_site = None
+		self.dark_current = None
+
+	def define_header_conventions():
+		pass
+
+	def update_values_from_header():
+		pass
+
+	def define_DANDIA_parameters() :
+		pass
+
+					
+
+class LCOSinistro(object):
+
+	def __init__(self):
+
+		self.name = None
+		self.type = 'Imager'	
+		self.camera_model = 'Sinistro'
+		self.pix_scale = 0.389
+		self.gain = 1.0
+		self.fov = 26.5*60
+		self.ADU_low = 100
+		self.ADU_high = 50000
+		self.ron = 12
+		self.ccd_temp = -100 # Celsius
+		self.define_header_conventions()
+
+	def define_header_conventions(self):
+		
+		self.header_dictionnary = {}
+		self.header_dictionnary['date_obs'] = 'DATE-OBS'
+		self.header_dictionnary['time_start'] = 'UT_START'
+		self.header_dictionnary['obs_type'] = 'OBSTYPE'
+		self.header_dictionnary['exp_time'] = 'EXPTIME'
+		self.header_dictionnary['filter1'] = 'FILTER1'
+		self.header_dictionnary['filter2'] = 'FILTER2'
+		self.header_dictionnary['filter3'] = 'FILTER3'
+		self.header_dictionnary['ra'] = 'RA'
+		self.header_dictionnary['dec'] = 'DEC'
+		self.header_dictionnary['reference_epoch'] = 'CAT-EPOC'
+		self.header_dictionnary['airmass'] = 'AIRMASS'	
+		self.header_dictionnary['sky_level'] = 'L1MEDIAN'
+		self.header_dictionnary['ron'] = 'RDNOISE' 	
+		self.header_dictionnary['ro_speed'] = 'RDSPEED'
+ 		self.header_dictionnary['telescope_site'] = 'SITEID'
+		self.header_dictionnary['instrument'] = 'INSTRUME'
+		self.header_dictionnary['binning'] = 'CCDSUM'
+		self.header_dictionnary['ccd_temp'] = 'CCDATEMP'
+
+	def update_values_from_header(self, image):
+		
+		from astropy.io import fits	
+		data = fits.open(image)
+		header = data[1].header
+
+		self.ron = header[self.header_dictionnary['ron']]
+	
+		binning = header[self.header_dictionnary['binning']]
+		self.x_binning = float(binning[0])
+		self.y_binning = float(binning[2])
+
+class fl03(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl03, self).__init__()
+		self.name = 'fl03'
+
+class fl04(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl04, self).__init__()
+		self.name = 'fl04'
+
+class fl05(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl05, self).__init__()
+		self.name = 'fl05'
+
+class fl06(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl06, self).__init__()
+		self.name = 'fl06'
+
+
+class fl08(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl08, self).__init__()
+		self.name = 'fl08'
+	
+class fl11(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl11, self).__init__()
+		self.name = 'fl11'
+		
+class fl12(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl12, self).__init__()
+		self.name = 'fl12'
+
+class fl14(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl14, self).__init__()
+		self.name = 'fl14'
+
+class fl15(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl15, self).__init__()
+		self.name = 'fl15'
+
+class fl16(LCOSinistro):
+	
+	def __init__(self) :
+
+		
+		super(fl16, self).__init__()
+		self.name = 'fl16'
